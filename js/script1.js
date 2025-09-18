77b1a9c63cc4cb1a664d1863c2667015b7907e23
Lau <laurafernandat5@gmail.com>
2025-09-18 15:38:25 -0400

    vista

--- js/script1.js
+++ js/script1.js
@@ -360,19 +360,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Abre el modal al hacer clic en el botón
   openModalBtn.addEventListener('click', function (event) {
-    event.preventDefault(); // Previene el comportamiento por defecto del enlace
-    modal.style.display = 'block';
+    event.preventDefault();
+    modal.classList.add('is-visible');
   });
 
   // Cierra el modal al hacer clic en el botón de cierre (X)
   closeModalBtn.addEventListener('click', function () {
-    modal.style.display = 'none';
+    modal.classList.remove('is-visible');
   });
 
   // Cierra el modal si el usuario hace clic fuera del contenido
   window.addEventListener('click', function (event) {
     if (event.target == modal) {
-      modal.style.display = 'none';
+      modal.classList.remove('is-visible');
     }
   });
 });
